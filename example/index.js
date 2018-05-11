213819cbc9e4b94efb3e9b02cfe1ec6b42eae642
Chris Dolphin <cdolphin1991@gmail.com>
2018-05-11 00:19:04 -0700

    0.1.8

--- example/index.js
+++ example/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import OverwatchTable from '../lib'
-import OverwatchSettingsSelect from 'overwatch-settings-select'
 
 const rows = [
     {
@@ -34,41 +33,8 @@ const props = {
 }
 
 
-class Example extends React.Component {
-
-    state = {
-        selectedIndex: 0,
-    }
-
-    onChange = (selectedIndex) => {
-        this.setState({
-            selectedIndex,
-        })
-    }
-
-    render() {
-        const { selectedIndex } = this.state
-
-        return <OverwatchSettingsSelect
-            selectedIndex={selectedIndex}
-            onChange={this.onChange}
-            options={
-                [
-                    'Easy',
-                    'Medium',
-                    'Hard',
-                    'Baller',
-                ]
-            }
-            label={'Difficulty'}
-        />
-    }
-}
-
-
 ReactDOM.render(
     <div>
-        <Example />
         <OverwatchTable
             {...props}
         />
