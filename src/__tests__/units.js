f83a41d0fd5c0a6d03fa814229884268efdc3a09
Chris Dolphin <cdolphin1991@gmail.com>
2018-05-11 18:01:29 -0700

    sortedRows and fullRows tests

--- src/__tests__/units.js
+++ src/__tests__/units.js
@@ -47,6 +47,24 @@ describe('fullRows', () => {
             fullRows
         )
 
+        test('if rows is an empty array, return empty array', () => {
+            expect(
+                fullRows([])
+            ).toEqual([])
+        })
+
+        test('if rows has length, but has no non-empty row objects, return empty array', () => {
+            expect(
+                fullRows([{}, {}, null, true])
+            ).toEqual([])
+        })
+
+        test('if rows has length and has full row objects, return an array of those row objects', () => {
+            expect(
+                fullRows([{some: 'key'}, {'some_other': 'value'}, true])
+            ).toEqual([{some: 'key'}, {'some_other': 'value'}])
+        })
+
     })
 })
 
@@ -74,6 +92,38 @@ describe('sortedRows', () => {
             expect(
                 sortedRows(['what', 'the', 5, { 'some': 'craziness' }], 'some_key')
             ).toEqual([{ 'some': 'craziness' }, 5, 'the', 'what'])
+        })
+
+        test('if fullRows and sortKey, and rows match that key, return fullRows sorted by that key', () => {
+            expect(
+                sortedRows([
+                    {some_key: 'alpha'},
+                    {some_key: 'the other one'},
+                    {some_key: 'beta'},
+                    {some_key: 'gamma'},
+                ], 'some_key')
+            ).toEqual([
+                {some_key: 'the other one'},
+                {some_key: 'gamma'},
+                {some_key: 'beta'},
+                {some_key: 'alpha'},
+            ])
+        })
+
+        test('if fullRows and sortKey and sortAscending, and rows match sortKey, return fullRows sorted by sortKey in reverse order', () => {
+            expect(
+                sortedRows([
+                    {some_key: 'alpha'},
+                    {some_key: 'the other one'},
+                    {some_key: 'beta'},
+                    {some_key: 'gamma'},
+                ], 'some_key', true)
+            ).toEqual([
+                {some_key: 'alpha'},
+                {some_key: 'beta'},
+                {some_key: 'gamma'},
+                {some_key: 'the other one'},
+            ])
         })
     })
 })
